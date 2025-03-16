304f40426171f2f164459660b8369dc5ef38b937
hasibulalam <hasibulalam108@gmail.com>
2025-03-16 11:09:31 -0700

    category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function list(){
+
+        $cat=Category::all();
+        return view('features.category.list',compact('cat'));
+    }
+
+
+    public function create(){
+        return view('features.category.create');
+       
+    } 
+
+
+    public function store(Request $request){
+     
+       Category::create([
+        "name" => $request->name,
+        "description" => $request->description,
+        "image" => $request->image,
+        "display_order" => $request->display_order,
+        "status" => $request->status
+        
+]);
+        
+        return redirect()->back();
+
+}
+
+}
